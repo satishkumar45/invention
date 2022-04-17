03fe0eae347f863402c468377453410c415836f7
satishkumar45 <sksatishkumar133@gmail.com>
2022-04-17 15:52:03 +0530

    Light:Focus on the earth     light observed by the earth.
    Signed-off-by: satishkumar45 <sksatishkumar133@gmail.com>

---
+++ devices/drivers/light/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   char light_focus_length_name,light_source_name;
+   int light_id,light_rays_length;
+   if(light_focus_length_name=="l")
+   {
+      printf("light falls on the earth by sun \n");
+   }
+   else
+   {
+     printf("light not recieved because sun is not came and also holiday for sun \n");
+   }
+
+}
